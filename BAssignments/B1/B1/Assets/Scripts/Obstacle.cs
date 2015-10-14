1649ea3563761939ade7f753dc1eadd1acad57ff
shilon <shilon@users.noreply.github.com>
2015-10-14 01:36:32 -0400

    Normalize all the line endings

---
+++ BAssignments/B1/B1/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+    public GameObject[] obstacles;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+            }
+            if (hit.transform.tag == "Obstacle")
+            {
+                obstacles = GameObject.FindGameObjectsWithTag("Obstacles");
+                for (int i =0; i < obstacles.Length; i++)
+                {
+                   // string obsVar = obstacles[i].GetComponent(ObstacleController);
+                }
+                Debug.Log("found obstacle");
+            }
+        }
+	}
+
+    
+}
