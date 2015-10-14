deaba2e6dfcd5ec80c9f26454077c60f7efae09a
shilon <shilon@users.noreply.github.com>
2015-10-14 01:35:06 -0400

    saving files before refreshing line endings

--- BAssignments/B1/B1/Assets/Scripts/ObstacleController.cs
+++ BAssignments/B1/B1/Assets/Scripts/ObstacleController.cs
@@ -18,8 +18,17 @@ public class ObstacleController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+        //rb.AddForce(movement * speed);
+
+        if (Mathf.Abs(moveHorizontal) > 0.0)
+        {
+            transform.position += Vector3.back * moveHorizontal * speed;
+        }
+        if (Mathf.Abs(moveVertical) > 0.0)
+        {
+            transform.position += Vector3.right * moveVertical * speed;
+        }
     }
 }
