7d428c6080de469dba2f67ceaf4b6f2987e712ea
shilon <shilon@users.noreply.github.com>
2015-11-23 12:48:23 -0500

    added handshake and more story

---
+++ BAssignments/B2/Assets/Handshake.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using RootMotion.FinalIK;
+
+public class Handshake : MonoBehaviour {
+
+    public class InteractionC2CDemo : MonoBehaviour
+    {
+
+        // GUI for testing
+
+        public InteractionSystem character1, character2; // The InteractionSystems of the characters
+        public InteractionObject handShake; // The HandShake InteractionObject
+
+        void LateUpdate(InteractionSystem character1, InteractionSystem character2, InteractionObject handShake)
+        {
+            // Positioning the handshake to the middle of the hands
+            Vector3 handsCenter = Vector3.Lerp(character1.ik.solver.rightHandEffector.bone.position, character2.ik.solver.rightHandEffector.bone.position, 0.5f);
+            handShake.transform.position = handsCenter;
+        }
+
+    }
+}
--- BAssignments/B2/Assets/MyBehaviorTreeWorking.cs
+++ BAssignments/B2/Assets/MyBehaviorTreeWorking.cs
@@ -5,16 +5,15 @@ using RootMotion.FinalIK;
 
 public class MyBehaviorTreeWorking : MonoBehaviour
 {
-	public Transform wander1;
-	public Transform wander2;
-	public Transform wander3;
+
     public Transform[] locations;
     public GameObject[] numberOfParticipants;
     public FullBodyBipedEffector[] eff;
     public InteractionObject[] obj;
     public InteractionObject[] objReset;
     public InteractionObject objects;
-
+    public InteractionSystem[] interacts;
+    public Transform[] searchPoints;
 
     private BehaviorAgent behaviorAgent;
 	// Use this for initialization
@@ -30,7 +29,7 @@ public class MyBehaviorTreeWorking : MonoBehaviour
 	{
 	
 	}
- 
+    
 
     protected Node Converse(int player1Index, int player2Index)
     {
@@ -86,8 +85,7 @@ public class MyBehaviorTreeWorking : MonoBehaviour
         numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().ST_PlayGesture("EAT", AnimationLayer.Face, 1000),
         new LeafWait(2000),
         numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_StartInteraction(effector, objReset));
-        //numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_WaitForFinish(effector),
-        //numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_StopInteraction(effector));
+        
 
     }
     protected Node ST_DanceBattle(int player1Index, int player2Index)
@@ -115,21 +113,25 @@ public class MyBehaviorTreeWorking : MonoBehaviour
 
     }
 
-    protected Node ST_ApproachAndWait(Transform target)
+    protected Node ST_ApproachAndWait(int player1Index, Transform target)
 	{
 		Val<Vector3> position = Val.V (() => target.position);
 		return new Sequence(
-             numberOfParticipants[0].GetComponent<BehaviorMecanim>().Node_GoTo(position), 
+             numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(position), 
             new LeafWait(1000));
 
 	}
 
-    protected Node ST_ShakeHands(Val<FullBodyBipedEffector> effector, Val<InteractionObject> obj, int player1Index)
+    protected Node ST_ShakeHands(InteractionObject handShake, Val<FullBodyBipedEffector> effector1, Val<FullBodyBipedEffector> effector2)
     {
-        
-        return new Sequence(
-            numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_StartInteraction(effector, obj));
+       
+        return new SequenceParallel(
+            numberOfParticipants[0].GetComponent<BehaviorMecanim>().Node_StartInteraction(effector1, handShake),
+            numberOfParticipants[1].GetComponent<BehaviorMecanim>().Node_StartInteraction(effector2, handShake),
+            new LeafWait(1000));
     }
+
+   
 
     protected Node ST_Gesture(int player1Index)
     {
@@ -139,24 +141,58 @@ public class MyBehaviorTreeWorking : MonoBehaviour
                 numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().ST_PlayGesture("CHEER", AnimationLayer.Hand, 3000),
                 new LeafWait(2000)));
     }
+    protected Node ST_LightSwitch(Val<FullBodyBipedEffector> effector, Val<InteractionObject> obj, int player1Index, Transform objpos)
+    {
+        Val<Vector3> objPos = Val.V(() => objpos.position);
+        return new Sequence(
+            numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(objPos),
+            numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_StartInteraction(effector, obj));
 
+    }
+    protected Node ST_Search( int player1Index)
+    {
+        
+        return new Sequence(
+            new SequenceShuffle(
+                numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(searchPoints[0].transform.position),
+                new LeafWait(1000),
+                numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(searchPoints[1].transform.position),
+                new LeafWait(1000),
+                numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(searchPoints[2].transform.position),
+                new LeafWait(1000),
+                numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(searchPoints[3].transform.position),
+                new LeafWait(1000),
+                numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(searchPoints[4].transform.position),
+                new LeafWait(1000),
+                numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(searchPoints[5].transform.position),
+                new LeafWait(1000),
+                numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(searchPoints[7].transform.position),
+                new LeafWait(1000),
+                numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(searchPoints[8].transform.position),
+                new LeafWait(1000),
+                numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(searchPoints[9].transform.position),
+                new LeafWait(1000),
+                numberOfParticipants[player1Index].GetComponent<BehaviorMecanim>().Node_GoTo(searchPoints[10].transform.position),
+                new LeafWait(1000)));
+                
+    }
 
 
     protected Node BuildTreeRoot()
     {
         int[] playerIndex = new int[numberOfParticipants.Length];
-        Val<Vector3>[] playerPos = new Val<Vector3>[numberOfParticipants.Length];
-
+        //Val<Vector3>[] playerPos = new Val<Vector3>[numberOfParticipants.Length];
+        //reshuffle(numberOfParticipants);
         for (int i = 0; i < playerIndex.Length; i++)
         {
             playerIndex[i] = i;
-            //[i] = Val.V(() => numberOfParticipants[i].transform.position);
+           
         }
-        //Val<Vector3> player1pos = Val.V(() => numberOfParticipants[playerIndex[0]].transform.position);
-        //Val<Vector3> player2pos = Val.V(() => numberOfParticipants[playerIndex[1]].transform.position);
+        //reshuffle(searchPoints);
 
         return
             new Sequence(
+               
                 new SequenceParallel(
                     new Sequence(
                         this.ST_ApproachAndOrient(locations[0], numberOfParticipants[0].gameObject.transform, playerIndex[0], locations[1], numberOfParticipants[1].gameObject.transform, playerIndex[1]),
@@ -171,13 +207,64 @@ public class MyBehaviorTreeWorking : MonoBehaviour
                             new SequenceParallel(
                                 this.ST_ApproachAndOrient(locations[6], numberOfParticipants[0].gameObject.transform, playerIndex[2], locations[7], numberOfParticipants[1].gameObject.transform, playerIndex[3]),
                                 this.ST_DanceBattle(playerIndex[0], playerIndex[1]),
+
                                 new SequenceParallel(
                                     this.ST_Gesture(playerIndex[2]),
                                     this.ST_Gesture(playerIndex[3]))))),
                     new Sequence(
                         this.ST_ApproachAndOrient(locations[10], numberOfParticipants[0].gameObject.transform, playerIndex[0], locations[11], numberOfParticipants[1].gameObject.transform, playerIndex[1]),
-                        this.ST_ShakeHands(eff[1], obj[6], playerIndex[0])));
+                        this.ST_ShakeHands(obj[6], eff[1], eff[1])),
+                new SequenceParallel(
+                    new SequenceShuffle(
+                        this.ST_ApproachAndWait(playerIndex[0], searchPoints[0]),
+                        this.ST_ApproachAndWait(playerIndex[0], searchPoints[3]),
+                        this.ST_ApproachAndWait(playerIndex[0], searchPoints[5])),
+                    new SequenceShuffle(
+                        this.ST_ApproachAndWait(playerIndex[1], searchPoints[1]),
+                        this.ST_ApproachAndWait(playerIndex[1], searchPoints[3]),
+                        this.ST_ApproachAndWait(playerIndex[1], searchPoints[2])),
+                    new SequenceShuffle(
+                        this.ST_ApproachAndWait(playerIndex[2], searchPoints[5]),
+                        this.ST_ApproachAndWait(playerIndex[2], searchPoints[7]),
+                        this.ST_ApproachAndWait(playerIndex[2], searchPoints[8])),
+                    new SequenceShuffle(
+                        this.ST_ApproachAndWait(playerIndex[3], searchPoints[9]),
+                        this.ST_ApproachAndWait(playerIndex[3], searchPoints[3]),
+                        this.ST_ApproachAndWait(playerIndex[3], searchPoints[5]))),
+                new SelectorParallel(
+                    this.ST_LightSwitch(eff[0], obj[7], playerIndex[0], searchPoints[6]),
+                    this.ST_LightSwitch(eff[0], obj[7], playerIndex[1], searchPoints[6]),
+                    this.ST_LightSwitch(eff[0], obj[7], playerIndex[2], searchPoints[6]),
+                    this.ST_LightSwitch(eff[0], obj[7], playerIndex[3], searchPoints[6])));
+                       
 
 
+    }
+    void reshuffle(Transform[] texts)
+    {
+        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        for (int t = 0; t < texts.Length; t++)
+        {
+            Transform tmp = texts[t];
+            int r = Random.Range(t, texts.Length);
+            texts[t] = texts[r];
+            texts[r] = tmp;
+        }
+    }
+
+  
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Trigger Event"))
+        {
+            
+      
+
+            
+            
+           
+
+        }
     }
 }
